3ef551934db14ba296c01e30201a534cbc18079c
LiamKirkland <liam.b.kirkland@gmail.com>
2026-06-23 11:37:46 -0400

    Complete Deliverables 5 & 6: Budget tracked, cannot over-purchase stock

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ const API = "http://localhost:3001/sushis";
 function App() {
   const [sushis, setSushis] = useState([])
   const [eatenSushis, setEatenSushis] = useState([])
+  const [budget, setBudget] = useState(150)
 
   function eatSushi(id) {
     setSushis(sushis.map(sushi => {
-      if(sushi.id === id) {
+      if(sushi.id === id && !sushi.eaten && sushi.price <= budget) {
         setEatenSushis([...eatenSushis, sushi])
+        setBudget(budget - sushi.price)
         return {
           ...sushi,
           eaten: !sushi.eaten
@@ -32,7 +34,7 @@ function App() {
   return (
     <div className="app">
       <SushiContainer sushis={sushis} onSetSushis={setSushis} eatSushi={eatSushi}/>
-      <Table plates={eatenSushis}/>
+      <Table plates={eatenSushis} budget={budget}/>
     </div>
   );
 }
