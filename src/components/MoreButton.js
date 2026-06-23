1152befb1567d73b78abf6d3ade9fdaba7f24202
LiamKirkland <liam.b.kirkland@gmail.com>
2026-06-23 10:53:39 -0400

    Complete Deliverable 3: Add 'More Sushi!' functionality

--- src/components/MoreButton.js
+++ src/components/MoreButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function MoreButton(props) {
-  return <button onClick={/* Fill me in! */ null}>More sushi!</button>;
+function MoreButton({sushiIndex, onSetIndex}) {
+  return <button onClick={() => onSetIndex(sushiIndex += 4)}>More sushi!</button>;
 }
 
 export default MoreButton;
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import MoreButton from "./MoreButton";
 import Sushi from "./Sushi"
 
-function SushiContainer({sushi, onSetSushi}) {
-  const sushiToDisplay = sushi.slice(0,4)
+function SushiContainer({sushi}) {
+  const [sushiIndex, setSushiIndex] = useState(0)
+  const sushiToDisplay = sushi.slice(sushiIndex, sushiIndex + 4)
 
+  console.log(sushiIndex)
+  console.log(sushi.slice(sushiIndex,4))
   console.log("sushitodisplay: ", sushiToDisplay)
   return (
     <div className="belt">
       {sushiToDisplay.map(sushi => <Sushi name={sushi.name} image={sushi.img_url} price={sushi.price} key={sushi.id} />)}
-      <MoreButton />
+      <MoreButton sushiIndex={sushiIndex} onSetIndex={setSushiIndex}/>
     </div>
   );
 }
